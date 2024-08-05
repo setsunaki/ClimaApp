9f1ef7c63c8d6b1c02a397067bf2954c6be76a44
setsunaki <wladimirburgos42@gmail.com>
2024-08-05 09:12:16 -0400

    Detalles temperatura e tipo de clima listo

--- src/app/left-container/left-container.component.ts
+++ src/app/left-container/left-container.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { faLocation } from '@fortawesome/free-solid-svg-icons';
+import { faCloud } from '@fortawesome/free-solid-svg-icons';
+import { faCloudRain } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-left-container',
@@ -11,6 +13,10 @@ import { faLocation } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './left-container.component.css'
 })
 export class LeftContainerComponent {
+  //Iconos
   faMagnifyingGlass = faMagnifyingGlass;
   faLocation = faLocation;
+  faCloud = faCloud;
+  faCloudRain = faCloudRain
+  //-------
 }
